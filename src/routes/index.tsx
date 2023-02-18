b72df894c09b194e1fd785aa4dac522ae4711500
1carlosrenan <carlosrenan96@gmail.com>
2023-02-17 23:15:44 -0300

    feat: adding theme and provider

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,14 @@ import { Routes, Route, Navigate } from "react-router-dom";
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={<Button>teste</Button>} />
+      <Route
+        path="/pagina-inicial"
+        element={
+          <Button variant="contained" color="primary">
+            teste
+          </Button>
+        }
+      />
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
   );

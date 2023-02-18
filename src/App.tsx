55e7f16da27625d7dcc1e8ecc9d598fb4976196a
1carlosrenan <carlosrenan96@gmail.com>
2023-02-17 22:33:08 -0300

    feat: adding react router dom v6 and its settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./routes";
+
 function App() {
-  return <div className="App">Teste</div>;
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,12 @@
-export {};
+import { Routes, Route, Navigate } from "react-router-dom";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/pagina-inicial" element={<p>Página Inicial</p>} />
+      <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
